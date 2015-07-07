227057bfb204ed1481c9f052d0f4ee3423e67bf4
shgtkshruch <shgtk.shruch@gmail.com>
2015-07-07 15:47:19 +0900

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+(function() {
+  $('.slick-normal').slick({
+    dots: true,
+    prevArrow: '<div class="slick-prev fa fa-angle-left"></div>',
+    nextArrow: '<div class="slick-next fa fa-angle-right"></div>'
+  });
+
+  $('.slick-autoplay').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<div class="slick-prev fa fa-angle-left"></div>',
+    nextArrow: '<div class="slick-next fa fa-angle-right"></div>'
+  });
+
+  $('.slick-fade').slick({
+    dots: true,
+    fade: true,
+    prevArrow: '<div class="slick-prev fa fa-angle-left"></div>',
+    nextArrow: '<div class="slick-next fa fa-angle-right"></div>'
+  });
+
+  $('.slick-sync').slick({
+    asNavFor: '.slick-nav'
+  });
+
+  $('.slick-nav').slick({
+    asNavFor: '.slick-sync',
+    slidesToShow: 5,
+    focusOnSelect: true
+  });
+
+}).call(this);
